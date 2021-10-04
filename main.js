c4ca8cc68649cde54f5b0046c07ebf73469c3812
suksa_ <suksa_@naver.com>
2021-10-04 14:36:27 +0900

    보완

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 var canvas = document.getElementById('canvas')
 var ctx = canvas.getContext('2d')
 
+var timer = 0
+var cactusLists = []
+var jumpTimer = 0
+var animation
+
 canvas.width = 600
-canvas.height = 600
+canvas.height = 250
 
 var dino = {
     x: 10,
@@ -28,11 +33,25 @@ class Cactus {
     }
 }
 
-var timer = 0
-var cactusLists = []
-var jumpTimer = 0
-var animation
-
+class End {
+    constructor() {
+        this.m1 = '당신은 우끼끼 티어'
+        this.m2 = '당신은 애니멀 티어'
+        this.m3 = '당신은 킹 티어'
+        this.m4 = '당신은 갓 티어'
+    }
+    draw() {
+        var m = ''
+        if (timer < 1000) m = this.m1
+        else if (timer < 1500) m = this.m2
+        else if (timer < 2000) m = this.m3
+        else m = this.m4
+        ctx.fillStyle = 'brown'
+        ctx.font = '40px serif';
+        var message = `${timer}점 ${m}`
+        ctx.fillText(message, 60, 90);
+    }
+}
 
 function re() {
     animation = requestAnimationFrame(re)
@@ -44,18 +63,17 @@ function re() {
     ctx.font = '20px serif';
     ctx.fillText(timer, 10, 30);
 
-    if (timer % 100 === 0) {
+    if (timer % 70 === 0) {
         var cactus = new Cactus()
         cactus.width = Math.random() * 50
         cactusLists.push(cactus)
     }
 
     cactusLists.forEach((a, i, o) => {
-        // x좌표가 0미만이면 제거
         if (a.x < 0) {
             o.splice(i, 1)
         }
-        a.x -= 2
+        a.x -= 3
 
         crash(dino, a)
         
@@ -63,15 +81,15 @@ function re() {
     })
 
     if (jumping === true) {
-        dino.y -= 3
+        dino.y -= 4
         jumpTimer++
     }
     if (jumping === false) {
         if (dino.y < 200) { 
-            dino.y += 3
+            dino.y += 4
         }
     }
-    if (jumpTimer > 40) {
+    if (jumpTimer > 30) {
         jumping = false
         jumpTimer = 0
     }
@@ -85,10 +103,12 @@ function crash(dino, cactus) {
     var x = cactus.x - (dino.x + dino.width)
     var y = cactus.y - (dino.y + dino.height)
     if (x < 0 && y < 0) {
-        console.log(dino, cactus)
+        var end = new End()
+        end.draw()
         cancelAnimationFrame(animation)
     }
 }
+
 
 var jumping = false
 document.addEventListener('keydown', (e) => {
